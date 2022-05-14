43d43f3b020bba5d31b703e200d3e70daffcbcaf
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-14 20:11:34 -0300

    refactor: new arch

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Devlulcas blog</title>
+				<meta name="description" content="A cool blog" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<h1>Hello world!</h1>
+		</div>
+	);
+}
