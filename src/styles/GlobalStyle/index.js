266955ebb4fa78aa37e852e6a7f1743ec389a698
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-15 01:53:09 -0300

    feat: fake default colors

--- src/styles/GlobalStyle/index.js
+++ src/styles/GlobalStyle/index.js
@@ -1,21 +1,32 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body {
+  * {
+    box-sizing: border-box;
     padding: 0;
     margin: 0;
+  }
+
+  html,
+  body {
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  
+  :root {
+    font-size: 62.5%;
+    --primary-light: #f0f0f0;
+    --primary: #c0c0c0;
+    --primary-dark: #999999;
+    
+    --secondary-light: #D5B741;
+    --secondary: #F5B741;
+    --secondary-dark: #D5A731;
   }
 
   a {
     color: inherit;
     text-decoration: none;
-  }
-
-  * {
-    box-sizing: border-box;
   }
 `;
 
