d778f9153e38481f0872e3012faa4cca76246b9d
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-15 03:33:05 -0300

    feat: loads the posts at build time

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import Head from "next/head";
+import { PostList } from "../components/elements/PostList";
+import { Default } from "../components/layouts/Default";
+import { getAllPosts } from "../model/repositories/Posts.ts";
 
-export default function Home() {
+export default function Home({ postsMeta }) {
 	return (
-		<div>
-			<Head>
-				<title>Devlulcas blog</title>
-				<meta name="description" content="A cool blog" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-
-			<h1>Hello world!</h1>
-		</div>
+		<Default>
+			<div>
+				<Head>
+					<title>Devlulcas blog</title>
+					<meta name="description" content="A cool blog" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+				<PostList postsMeta={postsMeta} />
+			</div>
+		</Default>
 	);
+}
+
+// Roda em tempo de compilação
+export async function getStaticProps() {
+	const posts = getAllPosts();
+	const postsToShow = posts.slice(0, 9);
+	const postsMeta = postsToShow.map((post) => post.meta);
+	console.log(postsMeta);
+
+	return {
+		props: { postsMeta },
+	};
 }
