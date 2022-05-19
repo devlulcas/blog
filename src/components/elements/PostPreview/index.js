2495823f55aeec5d93fce0b21c299c1fa9e42339
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-19 01:11:53 -0300

    feat: styles for the preview

--- src/components/elements/PostPreview/index.js
+++ src/components/elements/PostPreview/index.js
@@ -1,17 +1,43 @@
+import Image from "next/image";
 import Link from "next/link";
 import { TagsList } from "../TagsList";
+import { BsCalendarDateFill } from "react-icons/bs";
+
+import {
+	ImagePlaceholder,
+	DateContainer,
+	PostPreviewContainer,
+	PostTitle,
+	ExcerptContainer,
+} from "./styles";
 
 function PostPreview({ meta }) {
 	return (
 		<li>
-			<Link href={`/posts/${meta.slug}`}>
-				<h2>{meta.title}</h2>
-			</Link>
+			<PostPreviewContainer>
+				<Link href={`/posts/${meta.slug}`}>
+					<PostTitle>{meta.title}</PostTitle>
+				</Link>
 
-			<p>{meta.excerpt}</p>
+				<ImagePlaceholder title={meta.alt}>
+					<Image
+						alt={meta.alt}
+						src={meta.thumb}
+						width={40}
+						height={40}
+						layout={"fixed"}
+					/>
+				</ImagePlaceholder>
 
-			<time dateTime={meta.date}>{meta.date}</time>
-			<TagsList tags={meta.tags} />
+				<TagsList tags={meta.tags} />
+
+				<ExcerptContainer>{meta.excerpt}</ExcerptContainer>
+
+				<DateContainer title={`Postado em ${meta.date}`}>
+					<BsCalendarDateFill />
+					<time dateTime={meta.date}>{meta.date}</time>
+				</DateContainer>
+			</PostPreviewContainer>
 		</li>
 	);
 }
