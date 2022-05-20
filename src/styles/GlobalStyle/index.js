433191899bb3505aa121e3573ee4eb7f5d41c30f
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-20 02:29:03 -0300

    feat: portfolio colors

--- src/styles/GlobalStyle/index.js
+++ src/styles/GlobalStyle/index.js
@@ -10,8 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
   html,
   body {
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: 'Inter', sans-serif;
   }
   
   body {
@@ -20,13 +19,43 @@ const GlobalStyle = createGlobalStyle`
 
   :root {
     font-size: 62.5%;
-    --primary-light: #f0f0f0;
-    --primary: #c0c0c0;
-    --primary-dark: #999999;
-    
-    --secondary-light: #D5B741;
-    --secondary: #F5B741;
-    --secondary-dark: #D5A731;
+  
+    /* * PRIMARY COLORS */
+    --primary-h: 231;
+    --primary-s: 13%;
+    --primary-l: 11%;
+
+    --primary: hsl(var(--primary-h) var(--primary-s) var(--primary-l));
+    --primary-dark: hsl(var(--primary-h) var(--primary-s) 5%);
+    --primary-light: hsl(var(--primary-h) var(--primary-s) 31%);
+
+    /* * ON PRIMARY COLORS */
+    --on-primary-h: 0;
+    --on-primary-s: 0%;
+    --on-primary-l: 100%;
+
+    --on-primary: hsl(var(--on-primary-h) var(--on-primary-s) var(--on-primary-l));
+    --on-primary-dark: hsl(var(--on-primary-h) var(--on-primary-s) 85%);
+
+    /* * SECONDARY COLORS */
+    --secondary-h: 43;
+    --secondary-s: 100%;
+    --secondary-l: 54%;
+
+    --secondary: hsl(var(--secondary-h) var(--secondary-s) var(--secondary-l));
+    --secondary-dark: hsl(var(--secondary-h) var(--secondary-s) 5%);
+    --secondary-light: hsl(var(--secondary-h) var(--secondary-s) 61%);
+
+    /* * ON SECONDARY COLORS */
+    --on-secondary-h: var(--primary-h);
+    --on-secondary-s: var(--primary-s);
+    --on-secondary-l: var(--primary-l);
+
+    --on-secondary: hsl(var(--on-secondary-h) var(--on-secondary-s) var(--on-secondary-l));
+
+    /* * EXTRA COLORS */
+    --special: hsl(var(--secondary-h) var(--secondary-s) 80%);
+    --brand: #1947e5;
   }
 
   a {
