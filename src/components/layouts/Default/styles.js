1f2e871dd28b112e9b56070d887cd95084d41f58
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-20 02:29:30 -0300

    feat: background image

--- src/components/layouts/Default/styles.js
+++ src/components/layouts/Default/styles.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-	background-color: #000;
+	background-color: var(--primary-light);
 	min-height: 100vh;
+	background-image: url("data:image/svg+xml,%3Csvg width='42' height='44' viewBox='0 0 42 44' xmlns='http://www.w3.org/2000/svg'%3E%3Cg id='Page-1' fill='none' fill-rule='evenodd'%3E%3Cg id='brick-wall' fill='%23959595' fill-opacity='0.4'%3E%3Cpath d='M0 0h42v44H0V0zm1 1h40v20H1V1zM0 23h20v20H0V23zm22 0h20v20H22V23z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
 `;
 
 export { Container };
