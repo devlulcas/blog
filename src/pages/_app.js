ba6664a552e5801c46c20686a05647e3e959f9a7
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-20 02:28:07 -0300

    refactor: moves font loading to global document scope

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { GlobalStyle } from "../styles/GlobalStyle";
 import "../styles/highlight.css";
 
@@ -8,9 +7,6 @@ import "../styles/highlight.css";
 export default function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<Head>
-				<link rel="shortcut icon" href="/favicon.svg" />
-			</Head>
 			<GlobalStyle />
 			<Component {...pageProps} />
 		</>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,4 @@
-import Document from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 /**
@@ -30,5 +30,24 @@ export default class MyDocument extends Document {
 		} finally {
 			sheet.seal();
 		}
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500&family=Inter:wght@400;500;600;800&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
 	}
 }
