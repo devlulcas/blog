5cf16f29098f722fbf57c30a342801546ed6e0dc
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-20 22:54:33 -0300

    fix: crossorigin value

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -37,7 +37,11 @@ export default class MyDocument extends Document {
 			<Html>
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin={true}
+					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500&family=Inter:wght@400;500;600;800&display=swap"
 						rel="stylesheet"
