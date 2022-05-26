0eb01983c5a53511cb67a55b5108e7692c4005ce
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-25 22:45:49 -0300

    feat: add some styling

--- src/components/elements/Pic/index.js
+++ src/components/elements/Pic/index.js
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 function Pic({ src, alt, width, height, align }) {
 	return (
-		<div style={{ textAlign: align }}>
-			<Image src={`/pictures/${src}`} alt={alt} width={width} height={height} />
+		<div style={{ textAlign: align, margin: "1rem" }}>
+			<Image
+				style={{ borderRadius: ".5rem" }}
+				src={`/pictures/${src}`}
+				alt={alt}
+				width={width}
+				height={height}
+			/>
 		</div>
 	);
 }
