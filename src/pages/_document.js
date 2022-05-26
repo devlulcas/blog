013c75469ef36ab8424cf645a4b9b668926b6914
devlulcas <lucasalvesrego.contato@gmail.com>
2022-05-26 08:48:08 -0300

    deps: removes styled components

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,37 +1,9 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
 
 /**
- * Faz com que o styled componentes gere o CSS no servidor.
+ * Fontes globais.
  */
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) =>
-						sheet.collectStyles(<App {...props} />),
-				});
-
-			const initialProps = await Document.getInitialProps(ctx);
-
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
-
 	render() {
 		return (
 			<Html>
